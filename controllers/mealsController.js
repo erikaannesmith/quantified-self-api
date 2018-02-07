f5daa636ffb1f33bc2cd5c215c35f73a6a38c9c3
Emily Wise <wise.m.emily@gmail.com>
2018-02-07 15:52:26 -0700

    WIP added raw sql to join meals and foods but endpoint still not rendering

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,1 +1,15 @@
 var Meal = require('../models/meal')
+
+function show(req, res, next) {
+  let id = req.params.meal_id
+
+  Meal.find(id)
+  .then(meal => {
+    if (!meal) {
+      return res.sendStatus(404)
+    } else {
+      res.json(meal)
+    }
+  })
+}
+module.exports = {show}
--- models/meal.js
+++ models/meal.js
@@ -4,6 +4,15 @@ const database = require('knex')(configuration)
 
 
 var Meal = {
+find: function(id) {
+ return database.raw(`SELECT meals.name, meals.id, foods.name, foods.calories FROM meals
+                      INNER JOIN foodMeals on meals.id=foodmeals.meal
+                      INNER JOIN foods on foods.id=foodmeals.food WHERE meals.id=?`, [id])
+.then(function(meal) {
+    return meal.rows[0]
+})
+}
+
 
 }
 

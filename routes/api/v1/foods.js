79ff9ddc7196ed244bf0a5eb338e920b2efe71ee
Erika Smith <erikaannesmith@gmail.com>
2018-02-07 08:50:16 -0700

    refactor to create models and controllers for foods with one liner routes

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -3,15 +3,8 @@ const router = express.Router();
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
+const foodsController = require('../../../controllers/foodsController')
 
-
-router.get('/', function(req, res, next) {
-  database.raw('SELECT * FROM foods')
-  .then(foods => {
-    res.status(201).json(foods.rows)
-  });
-});
-
-
+router.get('/', foodsController.index);
 
 module.exports = router;

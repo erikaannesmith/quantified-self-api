279c0f9665c2c59fdef58b7db75e817905b1f349
Erika Smith <erikaannesmith@gmail.com>
2018-02-08 08:34:02 -0700

    begin to build out index function for meals, WIP

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -3,7 +3,7 @@ var Meal = require('../models/meal')
 function index(req, res, next) {
   Meal.all
   .then((meals) => {
-    
+    var pry = require('pryjs'); eval(pry.it)
     res.status(201).json(meals)
   })
 }
--- models/meal.js
+++ models/meal.js
@@ -3,9 +3,22 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 var Meal = {
-  all: database.raw('SELECT meals.id, meals.name, json_agg(foods.*) AS foods FROM meals JOIN foodMeals ON meals.id = foodMeals.meal JOIN foods ON foodMeals.food = foods.id GROUP BY meals.id')
-  .then(meals => {
-    return meals.rows
+  all:
+  database.raw('SELECT * FROM meals;')
+  .then((meals) => {
+    Promise.all(
+    meals.rows.map(function(meal) {
+      let id = meal.id
+      return database.raw('SELECT meals.id, meals.name, foods.* from meals join foodMeals ON meals.id = foodMeals.meal join foods on foods.id = foodMeals.food WHERE meals.id = ?;', [id])
+        .then(foods => {
+          let mealWithFoods = {id: meal.id, name: meal.name, foods: foods.rows}
+          return mealWithFoods
+        })
+      })
+    )
+    .then(x => {
+      return x
+    }) 
   })
 }
 

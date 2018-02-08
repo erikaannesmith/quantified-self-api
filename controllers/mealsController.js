08f3d6fe78eb2ac454a63ad8f7f7f24208f82e04
Emily Wise <wise.m.emily@gmail.com>
2018-02-08 11:36:01 -0700

    #154943548 all meals rendered

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -5,7 +5,6 @@ var Meal = require('../models/meal')
 function index(req, res, next) {
   Meal.all
   .then((meals) => {
-    var pry = require('pryjs'); eval(pry.it)
     res.status(201).json(meals)
   })
 }
--- models/meal.js
+++ models/meal.js
@@ -3,23 +3,24 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 var Meal = {
+
   all:
   database.raw('SELECT * FROM meals;')
   .then((meals) => {
-    Promise.all(
-    meals.rows.map(function(meal) {
-      let id = meal.id
-      return database.raw('SELECT meals.id, meals.name, foods.* from meals join foodMeals ON meals.id = foodMeals.meal join foods on foods.id = foodMeals.food WHERE meals.id = ?;', [id])
+    return Promise.all(
+      meals.rows.map(function(meal) {
+        let id = meal.id
+        return database.raw('SELECT meals.id, meals.name, foods.* from meals join foodMeals ON meals.id = foodMeals.meal join foods on foods.id = foodMeals.food WHERE meals.id = ?;', [id])
         .then(foods => {
           let mealWithFoods = {id: meal.id, name: meal.name, foods: foods.rows}
           return mealWithFoods
         })
       })
     )
-    .then(x => {
-      return x
+    .then(allmeals => {
+      return allmeals
     })
-  })
+  }),
 
   find: function(mealId) {
     return database.raw(`SELECT foods.id, foods.name, foods.calories FROM foods
